97a40bfb13d2d2d153560979c1e189a2b42d1e5d
mbolsius <70014849+mbolsius@users.noreply.github.com>
2020-11-18 17:32:31 -0500

    Add files via upload
    Created the class, object, and attributes for the players base set of hero attributes.

--- ATLiens.py
+++ ATLiens.py
@@ -40,3 +40,46 @@ class hero:
             self.magic = newMagic
         def setName(self, newName):
             self.name = newName
+            
+def createClass():
+	a = input("Are you more strategic (press 1) or more of a warrior (press 2)")
+	while a != "1" and a != "2"
+		print("invalid selection")
+            	a = input("Are you more strategic (press 1) or more of a warrior (press 2)...")
+
+	if a == "1":
+		heroAttack = 50
+		heroDefense = 100
+	elif a == "2"
+		heroAttack = 100
+		heroDefense = 50
+
+	b = input("Press enter to roll a dice...")
+	time.sleep(0.2)
+	print ("rolling dice")
+	heroLuck = random.randint(0,10)
+	print("your hero has", heroLuck, "luck out of 10")
+	
+	c = input("are you more of a bow and arrow user (press 1) or a magic user (press 2)")
+	while c != "1" and c != "2"
+		print("invalid selection")
+		c = input("are you more of a bow and arrow user (press 1) or a magic user (press 2)")
+
+	if c == "1"
+		heroRanged = 100
+		heroMagic = 50
+		
+	elif c == "2"
+		heroRanged = 50
+		heroMagic = 100
+		
+	heroName = input("What is your name hero")
+	print ("Welcome", heroName, "!!!")
+	
+	return (heroAttack, heroLuck, heroRanged, heroDefense, heroMagic, heroName)
+	
+class_data = createClass()
+
+character = hero(100, class_data[0], class_data[1], class_data[2], class_data[3], class_data[4], class_data[5], class_data[6])
+pprint(vars(character))
+	
